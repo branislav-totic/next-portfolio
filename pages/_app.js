b5d1dbc207f83c360fec873ad3a3e2e69978d42e
Totic Branislav <branislav.totic@gmail.com>
2021-03-24 22:32:47 +0100

    Add resize check and fix fonts loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+import react, { useState, useEffect } from "react";
 import Head from "next/head";
 import { Footer, Header } from "../components";
 import { CreateGlobalStyle, Contianer, Root } from "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  const handleResize = () => {
+    setIsDesktop(window.matchMedia("(min-width: 1024px)").matches);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
       <CreateGlobalStyle />
@@ -10,28 +24,28 @@ const MyApp = ({ Component, pageProps }) => {
         <title>NEXT App Test</title>
         <link rel="icon" href="/favicon.ico" />
         <link
-            rel="preload"
-            href="/fonts/GrimptPrint/Grimpt-PrintLight.woff"
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/GrimptPrint/Grimpt-PrintRegular.woff"
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/GrimptPrint/Grimpt-PrintBold.woff"
-            as="font"
-            crossOrigin=""
-          />
+          rel="preload"
+          href="/fonts/GrimptPrint/Grimpt-PrintLight.woff"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/GrimptPrint/Grimpt-PrintRegular.woff"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/GrimptPrint/Grimpt-PrintBold.woff"
+          as="font"
+          crossOrigin=""
+        />
       </Head>
       <Root>
-        <Header />
+        <Header isDesktop={isDesktop}/>
         <Contianer>
-          <Component {...pageProps} />
+          <Component {...pageProps} isDesktop={isDesktop} />
         </Contianer>
         <Footer />
       </Root>
