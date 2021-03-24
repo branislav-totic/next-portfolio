81e3f76383aaa1a58ab0417d3058a83985ba3493
Totic Branislav <branislav.totic@gmail.com>
2021-03-24 22:33:54 +0100

    Add few changes to Header

--- components/Header/Header.css.js
+++ components/Header/Header.css.js
@@ -9,11 +9,41 @@ export const Container = styled.header`
 `;
 
 export const Wrapper = styled.div`
-  position: relative;
+  background-color: #000;
+  color: #fff;
 `;
 
 export const Menu = styled.div`
   z-index: 1;
   width: 100%;
   padding: 15px 40px 10px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Hamburger = styled.div`
+  cursor: pointer;
+  display: inline-block;
+  overflow: hidden;
+  background: #000;
+  height: 20px;
+  width: 20px;
+`;
+
+export const HamburgerBox = styled.div``;
+
+export const HamburgerInner = styled.div``;
+
+export const ScrollBar = styled.div`
+  width: 100%;
+  height: 2px;
+  top: 0;
+  left: 0;
+  background: #cccccc;
+`;
+
+export const ScrollProgress = styled.div`
+  width: 0;
+  height: inherit;
+  background: linear-gradient(90deg, rgba(36,94,0,1) 0%, rgba(85,223,46,1) 100%);
 `;
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,15 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Link from "next/link";
 import MobileMenu from "./MobileMenu/MobileMenu";
-import { Container, Menu, Wrapper } from "./Header.css";
+import {
+  Container,
+  Hamburger,
+  HamburgerBox,
+  HamburgerInner,
+  Menu,
+  ScrollBar,
+  ScrollProgress,
+  Wrapper,
+} from "./Header.css";
 
-const Header = () => {
+const Header = ({ isDesktop }) => {
   const [isMobileOpen, setIsMobileOpen] = useState(false);
+  const scrollRef = useRef()
+
+  useEffect(() => {
+    if (isMobileOpen) {
+      document.body.style.overflowY = `hidden`;
+    } else {
+      document.body.style.overflowY = `auto`;
+    }
+  }, [isMobileOpen]);
+
+  const indicateScrollBar = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const distanceFromPageTop = document.body.scrollTop || scrollTop;
+    const height = scrollHeight - clientHeight;
+    const scrolled = (distanceFromPageTop / height) * 100;
+    scrollRef.current.style.width = `${scrolled}%`;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", indicateScrollBar);
+    return () => window.removeEventListener("scroll", indicateScrollBar);
+  }, []);
+
+  const handleMenuClick = () => {
+    setIsMobileOpen(!isMobileOpen);
+  };
+
   return (
     <Container>
       <Wrapper>
-        <Menu onClick={() => setIsMobileOpen(!isMobileOpen)}>{`< Branislav Totic />`}</Menu>
-        <MobileMenu isHidden={isMobileOpen} />
+        <Menu>
+          <Link href="/">{`< Branislav Totić />`}</Link>
+          {!isDesktop && (
+            <Hamburger onClick={handleMenuClick}>
+              <HamburgerBox>
+                <HamburgerInner isActive={!isMobileOpen} />
+              </HamburgerBox>
+            </Hamburger>
+          )}
+        </Menu>
+        <ScrollBar>
+          <ScrollProgress ref={scrollRef}/>
+        </ScrollBar>
       </Wrapper>
+      {!isDesktop && <MobileMenu isHidden={isMobileOpen} />}
     </Container>
   );
 };
